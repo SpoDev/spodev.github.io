59305131e09281ccdd1318935579723d47fb9f04
Karol Gorecki <hi@karolgorecki.pl>
2017-01-20 23:07:21 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,13 @@
-
 importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-messaging.js');
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAlMNVGCyaUyHP1Xra6eT5FSaem_bEbsbc",
+  authDomain: "notify-81e4e.firebaseapp.com",
+  databaseURL: "https://notify-81e4e.firebaseio.com",
+  storageBucket: "notify-81e4e.appspot.com",
+  messagingSenderId: "706359507600"
+};
+
+firebase.initializeApp(config);
